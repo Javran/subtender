d1a6793816bd037a6f27043fd572588513613c72
Javran Cheng <Javran.c@gmail.com>
2017-07-22 22:50:38 -0400

    merge scripts into package.json

--- check.sh
+++
@@ -1,8 +0,0 @@
-# for some environments "node" is lazily loaded,
-# therefore it's preferred to source this file instead of executing it
-# e.g. ". ./check.sh"
-
-# make sure it's properly loaded
-node --version
-
-npm test && npm run lint
