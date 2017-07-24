652a4ec5d3d9d91b63ef2d71fd6d50e9389a638b
Javran Cheng <Javran.c@gmail.com>
2017-07-24 18:05:59 -0400

    mkDebug for poi

--- src/poi.js
+++ src/poi.js
@@ -24,20 +24,84 @@ const selectorTester =
   }
 
 const injectReloaders = () => {
-  window.clearTaigeiCache = () => {
+  const clearCacheWithKeyword = keyword => {
+    const keys =
     Object.keys(require.cache)
-      .filter(x => x.indexOf('subtender') !== -1)
-      .map(x => delete require.cache[x])
+      .filter(x => x.indexOf(keyword) !== -1)
+
+    keys.map(x => delete require.cache[x])
+    // eslint-disable-next-line no-console
+    console.log('cleared keys: ', keys)
   }
 
-  window.clearI18nCache = () => {
-    Object.keys(require.cache)
-      .filter(x => x.indexOf('i18n') !== -1)
-      .map(x => delete require.cache[x])
+  window.clearCacheWithKeyword = clearCacheWithKeyword
+  window.clearTaigeiCache = () =>
+    clearCacheWithKeyword('subtender')
+
+  window.clearI18nCache = () =>
+    clearCacheWithKeyword('i18n')
+}
+
+const mkDebug = (tagName='subtender',startEnabled=false) => {
+  const { dbg } = window
+  const dbgHandler = dbg.extra(tagName)
+
+  if (startEnabled) {
+    dbgHandler.enable()
+  } else {
+    dbgHandler.disable()
   }
+
+  /*
+     dbgHandler.log and dbgHandler.assert cannot be bound too early,
+     otherwise the enable() / disable() won't be respected.
+
+     we workaround by using class getters, which can be used without
+     explicit giving an argument list.
+
+     this way runtime dbgHandler 'enabled' flag will be respected,
+        and also we'll get correct source locations
+   */
+  const debug = new (class {
+    constructor(dbgHandlerArg) {
+      this.dbgHandler = dbgHandlerArg
+    }
+
+    get log() {
+      return this.dbgHandler.log
+    }
+
+    get assert() {
+      return this.dbgHandler.assert
+    }
+
+    enable() {
+      return this.dbgHandler.enable()
+    }
+
+    disable() {
+      return this.dbgHandler.disable()
+    }
+    // things that could go wrong should be easily spotted
+    // unconditionally
+    warn =
+      console.warn.bind(
+        console,
+        `%c${tagName}`,
+        'background: linear-gradient(30deg, cyan, white 3ex)')
+
+    error =
+      console.error.bind(
+        console,
+        `%c${tagName}`,
+        'background: linear-gradient(30deg, cyan, white 3ex)')
+  })(dbgHandler)
+
+  return debug
 }
 
 export {
   selectorTester,
   injectReloaders,
+  mkDebug,
 }
